fa7d0ac60d35c597c3c1cbdc1e3943792157aa4c
rmm72 <rmm72@users.noreply.github.com>
2019-11-19 05:35:49 -0500

    Mechanic implemented
    Drag and drop works on the Packing scene now, woohoo!

---
+++ Grown/Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    private bool mouseDown;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (mouseDown)
+        {
+            rb.velocity = new Vector2(0, 0);
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objectPosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        mouseDown = true;
+    }
+    private void OnMouseUp()
+    {
+        mouseDown = false;
+    }
+}
