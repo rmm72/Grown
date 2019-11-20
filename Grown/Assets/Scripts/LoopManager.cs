2288dd569588480bcb96f08963d78e9673ed5876
rmm72 <rmm72@users.noreply.github.com>
2019-11-20 12:26:15 -0500

    Asset tweaks
    Assets moved around and scripts tweaked.

---
+++ Grown/Assets/Scripts/LoopManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopManager : MonoBehaviour
+{
+    public bool loop1;
+    public bool loop2;
+    public bool loop3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        loop1 = true;
+        loop2 = false;
+        loop3 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Grown/Assets/Scripts/MoveCharacter.cs
+++ Grown/Assets/Scripts/MoveCharacter.cs
@@ -4,33 +4,91 @@ using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour
 {
-    private float positionX;
-    public GameObject levelTwo;
-    public GameObject levelThree;
+    public float positionX;
+    //public GameObject levelTwo;
+    //public GameObject levelThree;
     public GameObject character;
-    private void Start()
+    public bool walk1;
+    public bool walk2;
+    public bool walk3;
+
+    public void Start()
     {
-        positionX = -7f;
+        positionX = -3.24f;
+        walk1 = true;
+        walk2 = false;
+        walk3 = false;
     }
     void Update()
     {
-        if(positionX > -7f && positionX < 8f)
+        if (walk1 == true && walk2 == false && walk3 == false)
         {
-            positionX -= 0.5f * Time.deltaTime;
-        }
-        else if (positionX <= -7f)
+            if (positionX > -3.24f && positionX < 4.24f)
+            {
+                positionX -= 0.15f * Time.deltaTime;
+            }
+            else if (positionX <= -3.24f)
+            {
+                positionX = -3.24f;
+            }
+            else if (positionX >= 4.24f)
+            {
+                //levelTwo.SetActive(false);
+                //levelThree.SetActive(true);
+                walk1 = character.GetComponent<LoopManager>().loop1 = false;
+                walk2 = character.GetComponent<LoopManager>().loop2 = true;
+                walk3 = character.GetComponent<LoopManager>().loop3 = false;
+                Debug.Log("Loop 1 done.");
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                positionX += 0.75f;
+            }
+        } else if (walk1 == false && walk2 == true && walk3 == true)
         {
-            positionX = -7f;
-        }
-        else if (positionX >= 8f)
+            if (positionX > -3.24f && positionX < 4.24f)
+            {
+                positionX -= 0.05f * Time.deltaTime;
+            }
+            else if (positionX <= -3.24f)
+            {
+                positionX = -3.24f;
+            }
+            else if (positionX >= 4.24f)
+            {
+                //levelTwo.SetActive(false);
+                //levelThree.SetActive(true);
+                walk1 = character.GetComponent<LoopManager>().loop1 = false;
+                walk2 = character.GetComponent<LoopManager>().loop2 = false;
+                walk3 = character.GetComponent<LoopManager>().loop3 = true;
+                Debug.Log("Loop 2 done.");
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                positionX += 1.15f;
+            }
+        } else if (walk1 == false && walk2 == false && walk3 == true)
         {
-            levelTwo.SetActive(false);
-            levelThree.SetActive(true);
-        }
-        if (Input.GetMouseButtonDown(0)){
-            positionX += .5f;
+            if (positionX > -3.24f && positionX < 4.24f)
+            {
+                positionX -= 0.0f * Time.deltaTime;
+            }
+            else if (positionX <= -3.24f)
+            {
+                positionX = -3.24f;
+            }
+            else if (positionX >= 4.24f)
+            {
+                //levelTwo.SetActive(false);
+                //levelThree.SetActive(true);
+                Debug.Log("Loop 3 done.");
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                positionX += 1.75f;
+            }
         }
        
-        character.transform.position = new Vector3(positionX, 0f, 0f);
+        character.transform.position = new Vector3(positionX, -1.5f, 0f);
     }
 }
